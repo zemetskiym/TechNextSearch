d888e65521cd953d651e1a802a2d82f60bff5f04
zemetskiym <zemetskiym@proton.me>
2023-11-16 17:58:17 -0800

    Feat: add database with full-text search

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/zemetskiym/TechNextSearch
 
 go 1.21.4
+
+require github.com/mattn/go-sqlite3 v1.14.18 // indirect
--- main.go
+++ main.go
@@ -1,14 +1,26 @@
 package main
 
 import (
-    "strings"
-    "net/http"
-    "encoding/json"
+	"database/sql"
+	"encoding/json"
+	"log"
+	"net/http"
+	"strings"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 type Response struct {
-    Message string `json:"message"`
+    Message []Row `json:"message"`
     Status int `json:"status"`
+}
+
+type Row struct {
+    Idx int
+    Idx2 int
+    PatentId int
+    PatentText string
+    Phase string
+    Date string
 }
 
 func main() {
@@ -24,21 +36,64 @@ func main() {
 
 func handleSearch (w http.ResponseWriter, r *http.Request) {
     // Check for query parameters
-    queryParams := r.URL.Query()
-    if len(queryParams) == 0 {
-        http.Error(w, "Missing query parameters", http.StatusBadRequest)
+    queryString := r.URL.Query().Get("query")
+    
+    // Convert the query string to be used to query database
+    queryString = strings.ReplaceAll(queryString, " ", " + ")
+
+    // Open a database connection
+    db, err := sql.Open("sqlite3", "./database/patents.db")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer db.Close()
+
+    // Define the parameterized query to perform
+    query := "SELECT * FROM virtual_patents_table WHERE patent_text MATCH ? ORDER BY rank;"
+
+    // Perform match query on virtual table for full-text search
+    rows, err := db.Query(query, queryString)
+    if err != nil {
+        log.Printf("Error querying database: %v", err)
+        http.Error(w, "Error querying database", http.StatusInternalServerError)
         return
     }
+    defer rows.Close()
 
-    // Get a slice of values for the "query" key
-    queryKeys := make([]string, 0, len(queryParams))
-    for key, _ := range queryParams {
-        queryKeys = append(queryKeys, key)
+    // Initialize slice to hold row data
+    var results []Row
+
+    // Iterate over the rows
+    for rows.Next() {
+        // Create variables to store column values
+        var Idx, Idx2, PatentId int
+        var PatentText, Phase, Date string
+
+        // Scan the values from the row into variables
+        err := rows.Scan(&Idx, &Idx2, &PatentId, &PatentText, &Phase, &Date)
+        if err != nil {
+            log.Printf("Error scanning row: %v", err)
+            http.Error(w, "Error scanning row", http.StatusInternalServerError)
+            return
+        }
+
+        // Create a struct instance and populate it with the values
+        result := Row{
+            Idx: Idx,
+            Idx2: Idx2,
+            PatentId: PatentId,
+            PatentText: PatentText,
+            Phase: Phase,
+            Date: Date,
+        }
+
+        // Append the struct to the results data
+        results = append(results, result)
     }
-
+    
     // Construct the response struct
     response := Response{
-        Message: strings.Join(queryKeys, " "),
+        Message: results,
         Status: http.StatusOK,
     }
 
